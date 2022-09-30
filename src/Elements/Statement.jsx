edb54d275877ff297455d477c17a97e5abf311ba
erdodif <erdodif@gmail.com>
2022-09-30 13:14:26 +0200

    Text in state, default text for Start

--- src/Elements/Statement.jsx
+++ src/Elements/Statement.jsx
@@ -6,13 +6,18 @@ export default class Statement extends Component {
 
     constructor(props) {
         super(props);
+        let text = null;
+        if (props.text !== undefined) {
+            text = props.children;
+        }
         let children = null;
-        if (props.children !== undefined) {
-            children = props.children;
+        if (props.children !== undefined && props.children !== null) {
+            children = props.children
         }
         this.state = {
             button: this.getButton(),
             children: children,
+            text: text,
         }
     }
 
@@ -21,25 +26,25 @@ export default class Statement extends Component {
         this.setState({ button: this.getButton() });
     }
 
-    addChildren = () => {
-        let children = this.state.children;
-        this.setState({children: children.push(<Statement key={children.length.toString}/>)});
+    addChildren = (children = this.state.children) => {
+        if (children === undefined || children === null) return;
+        this.setState({ children: (<Statement key={this.prop.key + 1}>{children}</Statement>)});
     }
 
     getMainPart = () => {
         switch (this.props.main) {
             case 'start':
+                return "START";
+            case 'if':
+                return "IF STATEMENT"
             default:
-                return 'START';
+                return this.state.text??"EMPTY STATEMENT";
         }
     }
 
     getButton = () => {
-        /*if (this.state.children.length === 0) {
-            return (<div>+</div>);
-        }*/
         let buttons = [<div className="action-button" onClick={this.clearChildren}>X</div>];
-        buttons.push(<div className="action-button" onClick={this.clearChildren}>+</div>);
+        buttons.push(<div className="action-button" onClick={this.addChildren}>+</div>);
         return (
             <div className="buttons">
                 {buttons}
@@ -49,6 +54,7 @@ export default class Statement extends Component {
 
     render() {
         return (
+
             <div className={`statement ${this.props.main}`}>
                 <div className="main">
                     {this.getMainPart()}
@@ -56,7 +62,7 @@ export default class Statement extends Component {
                 <div className="content">
                     {this.state.children}
                 </div>
-                    {this.state.button}
+                {this.state.button}
             </div>
         );
     }
